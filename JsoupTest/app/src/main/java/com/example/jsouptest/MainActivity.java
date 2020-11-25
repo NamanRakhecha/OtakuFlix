e286330d3f4553cb865dda8ce05d1970ab00c60c
Shubham <shubham30.sm@gmail.com>
2020-11-25 10:54:59 +0530

    Fixed Design issues. Added Default Dark Mode. 	modified:   JsoupTest/app/src/main/java/com/example/jsouptest/Adapter.java 	modified:   JsoupTest/app/src/main/java/com/example/jsouptest/MainActivity.java 	modified:   JsoupTest/app/src/main/res/layout/activity_main.xml 	modified:   JsoupTest/app/src/main/res/layout/anime_titles.xml 	modified:   JsoupTest/app/src/main/res/values/preloaded_fonts.xml 	JsoupTest/app/src/main/res/drawable/frame_border.xml 	JsoupTest/app/src/main/res/font/.xml 	JsoupTest/app/src/main/res/font/days_one.ttf 	JsoupTest/app/src/main/res/font/noto_sans_bold.xml 	JsoupTest/app/src/main/res/font/pacifico.ttf 	JsoupTest/app/src/main/res/font/paprika.ttf 	JsoupTest/app/src/main/res/font/roboto_black.ttf

--- JsoupTest/app/src/main/java/com/example/jsouptest/MainActivity.java
+++ JsoupTest/app/src/main/java/com/example/jsouptest/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.jsouptest;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -41,10 +43,11 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
 
 
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         recyclerView = (RecyclerView) findViewById(R.id.Main_Recycler);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
 
         List list = new List();
         list.execute();
@@ -54,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     public class List extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
@@ -62,43 +66,43 @@ public class MainActivity extends AppCompatActivity {
                 Document document = Jsoup.connect("https://gogoanime.so/").get();
                 Elements img = document.select("div.img");
                 Elements title = document.select("p.name");
-                Elements episode_no= document.select("p.episode");
+                Elements episode_no = document.select("p.episode");
 
 
-               for(int i =0; i<title.size(); i++){
-                   String extract_link = title.select("a").eq(i).attr("abs:href");
-                   vidLinkList.add(extract_link);
+                for (int i = 0; i < title.size(); i++) {
+                    String extract_link = title.select("a").eq(i).attr("abs:href");
+                    vidLinkList.add(extract_link);
 
-               }
-               for (int i= 0 ; i<vidLinkList.size();i++){
-                   Document videoLinkDoc= Jsoup.connect(vidLinkList.get(i).toString()).get();
+                }
+                for (int i = 0; i < vidLinkList.size(); i++) {
+                    Document videoLinkDoc = Jsoup.connect(vidLinkList.get(i).toString()).get();
 
-                   String vidStreamUrl = videoLinkDoc.getElementsByClass("play-video").get(0).getElementsByTag("iframe").get(0).attr("abs:src");
-                   vidStreamUrl = vidStreamUrl.replaceAll("streaming.php","ajax.php");
+                    String vidStreamUrl = videoLinkDoc.getElementsByClass("play-video").get(0).getElementsByTag("iframe").get(0).attr("abs:src");
+                    vidStreamUrl = vidStreamUrl.replaceAll("streaming.php", "ajax.php");
 
-                      vidUrl.add(vidStreamUrl);
+                    vidUrl.add(vidStreamUrl);
 
-               }
-               for (int i=0; i<vidUrl.size();i++){
-                   try {
-                       Document page = Jsoup.connect(vidUrl.get(i).toString()).ignoreContentType(true).get();
-                       JSONObject jsonObject = new JSONObject(page.text());
-                       String qualityUrl = ((JSONObject)jsonObject.getJSONArray("source").get(0)).getString("file");
+                }
+                for (int i = 0; i < vidUrl.size(); i++) {
+                    try {
+                        Document page = Jsoup.connect(vidUrl.get(i).toString()).ignoreContentType(true).get();
+                        JSONObject jsonObject = new JSONObject(page.text());
+                        String qualityUrl = ((JSONObject) jsonObject.getJSONArray("source").get(0)).getString("file");
 
-                       getLink.add(qualityUrl);
+                        getLink.add(qualityUrl);
 
-                   } catch (Exception e) {
-                       e.printStackTrace();
-                   }
-               }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
 
                 int size = img.size();
-                for(int i=0; i<size; i++) {
+                for (int i = 0; i < size; i++) {
                     String imgUrl = img.select("div.img").select("img").eq(i).attr("src");
                     String animetitle = title.select("a").eq(i).attr("title").toString();
-                    String episode= episode_no.eq(i).text();
+                    String episode = episode_no.eq(i).text();
                     String vidLink = getLink.get(i).toString();
-                    mList.add(new AnimeModel(imgUrl,animetitle,vidLink,episode));
+                    mList.add(new AnimeModel(imgUrl, animetitle, vidLink, episode));
                 }
 
 
@@ -107,7 +111,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
-
 
 
         @Override
